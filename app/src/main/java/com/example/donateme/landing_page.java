e40f326b7f7ce43b7130ef7337f2f6e2873896e8
Grandeur77 <fernandoshamen77@gmail.com>
2025-01-10 00:32:57 +0530

    create dashboard

--- app/src/main/java/com/example/donateme/landing_page.java
+++ app/src/main/java/com/example/donateme/landing_page.java
@@ -1,5 +1,6 @@
 package com.example.donateme;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -10,12 +11,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class landing_page extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_landing_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cardMyPin), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
